55b39b97670728501fff6ff5592631384cf2fd66
Alexis Pain <apain@excellium-services.lu>
2021-10-06 17:38:17 +0200

    Fixing different mistakes regarding special requests

--- FUSSER.py
+++ FUSSER.py
@@ -52,20 +52,20 @@ def execute_request(word, special, url, method, data, headers, proxy, ssl, regex
     if regex:
         regexp = re.compile(regex)
         if regexp.search(response.text):
-            print(' Pattern: Match')
+            print(' | Pattern: Match')
         else:
-            print(' Pattern: Do not match')
+            print(' | Pattern: Do not match')
     else:
         print()
 
 def execute_special_request(url, method, data, headers, proxy, ssl, regex):
     regexp = re.compile(regex)
-    response = method(url=URL, data=data, headers=headers, proxies=proxy, verify=ssl)
+    response = method(url=url, data=data, headers=headers, proxies=proxy, verify=ssl)
     text = response_to_string(response)
     try:
-        res = regexp.search(test)[0]
-    except IndexError:
-        print("Special string not found")
+        res = regexp.search(text)[0]
+    except TypeError:
+        print("Special string not found in the server's response")
         exit()
     return res
 
@@ -85,8 +85,8 @@ parser.add_argument('-H', '--header', help='A header to add to the requests', re
 parser.add_argument('-P', '--pattern', help='A regex to check against the body of the responses of the server', required=False, default=None)
 
 # Regular task
-parser.add_argument('-SD', '--special-delay', help='The delay to wich the special task is to be performed', required=False, default=-1)
 parser.add_argument('-Su', '--special-url', help='The URL of the special task', required=False, default=None)
+parser.add_argument('-SD', '--special-delay', help='The delay to wich the special task is to be performed (in seconds)', required=False, default=None)
 parser.add_argument('-Sm', '--special-method', help='The HTTP method to use for the special task', required=False, default="GET")
 parser.add_argument('-Sd', '--special-data', help='The data in the body of the requests for the special task', required=False, default=None)
 parser.add_argument('-SH', '--special-header', help='A header to add to the requests for the special task', required=False, action='append', default=None)
@@ -103,26 +103,32 @@ if args['proxy'] != 'None':
             }
 method = get_request_method(args['method'])
 special_method = get_request_method(args['special_method'])
-special_delay = int(args['special_delay'])
 ignore_ssl = not args['ignore_ssl'].lower() == 'true' # The variable name is bad, it should be something like 'perform_ssl_checks'
 if not ignore_ssl:
     requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 headers = parse_headers(args['header'])
+special_headers = parse_headers(args['special_header'])
 
 # Fuzzing
 with open(args['wordlist'], 'r') as wordlist:
 #   with concurrent.future.ThreadPoolExecutor(max_workers = threads) as executor:
     if args['special_url']:
-        special = execute_special_request(args['special_url'], special_method, args['special_data'], args['special_header'], proxy, ignore_ssl, args['special_pattern'])
+        if not args['special_delay']:
+            print('A delay is needed when providing a special URL')
+            exit()
+        if not args['special_pattern']:
+            print('A pattern is needed when providing a special URL')
+            exit()
+        special = execute_special_request(args['special_url'], special_method, args['special_data'], special_headers, proxy, ignore_ssl, args['special_pattern'])
         last_special = time.time()
+        special_delay = int(args['special_delay'])
     else:
         special = None
     word = wordlist.readline()
     while word:
-        if word[-1] == '\n':
-            word = word[:-1]
-        if special and time.time() - last_special < special_delay:
-            special = execute_special_equest(args['special_url'], special_method, args['special_data'], args['special_header'], proxy, ignore_ssl, args['special_pattern'])
+        word = word[:-1] if word[-1] == '\n' else word
+        if special and time.time() - last_special > special_delay:
+            special = execute_special_request(args['special_url'], special_method, args['special_data'], special_headers, proxy, ignore_ssl, args['special_pattern'])
             last_special = time.time()
         execute_request(word, special, args['url'], method, args['data'], headers, proxy, ignore_ssl, args['pattern'])
         word = wordlist.readline()
