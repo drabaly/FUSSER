5311da97e1205bd53dbf1ba549dfe446ada0f134
Alexis Pain <apain@excellium-services.lu>
2022-04-13 21:43:36 +0200

    Adding locks to avoid race conditions

--- src/listIterator.py
+++ src/listIterator.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
+import threading
+
 class Iterator:
     def __init__(self, filename):
         self.wordlist = open(filename, 'r')
 
+        self.lock = threading.Lock()
+
     def get_next_element(self):
+        self.lock.acquire()
         result = self.wordlist.readline()
+        self.lock.release()
         result = result[:-1] if result and result[-1] == '\n' else result
         return result
 
